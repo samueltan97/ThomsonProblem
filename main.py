d57e7a1287064fa3c8a7828c60a3973bc0311441
Samuel <samueltan97@hotmail.com>
2020-06-21 11:35:52 -0400

    Added pot energy calculation and positions

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ class MainCycle:
         self.particle_count = int(particle_count)
         self.delta_t = float(delta_t)
         self.counter = np.array([0])
+        self.total_potential_energy = []
+        self.min_potential_energy_pos = []
         self.make_particle_list()
 
     def call_at_interval(self, period, callback, args):
@@ -23,6 +25,8 @@ class MainCycle:
             self.counter = np.append(self.counter, current_counter)
             sleep(period)
             callback(*args)
+
+        print('done')
 
 
     def set_interval(self, period, callback, *args):
@@ -38,7 +42,9 @@ class MainCycle:
     def set_positions(self):  # sets random INITIAL positions of particles
         for i in range(len(self.particle_list)):
             set_pos = [np.random.rand(3)]
+            # set_pos = [0.3*i, 0.3*i, 0.3*i]
             self.particle_list[i].pos = (set_pos/np.linalg.norm(set_pos))*1.01
+        self.calc_potential_energy(self.particle_list)
 
     def plot_sphere(self):
         x = 1 * np.outer(np.cos(phi), np.sin(theta))
@@ -66,9 +72,19 @@ class MainCycle:
                 sep = particle_list[i].pos[-1] - particle_list[j].pos[-1]
                 radius = particle_list[i].pos[-1]
                 if i!=j:
-                    force = sep / (np.linalg.norm(sep)) ** 3
+                    force = particle_list[j].charge * particle_list[i].charge * sep / (np.linalg.norm(sep)) ** 3
                     total_force = total_force + (force - np.dot(force, radius)*(radius/np.linalg.norm(radius)))
             particle_list[i].force = np.vstack((particle_list[i].force, total_force))
+
+    def calc_potential_energy(self, particle_list):
+        total_potential_energy = 0
+        for i in range(len(particle_list)):
+            for j in range(i+1, len(particle_list)):
+                sep = particle_list[i].pos[-1] - particle_list[j].pos[-1]
+                potential_energy = particle_list[j].charge * particle_list[i].charge /np.linalg.norm(sep)
+                total_potential_energy += potential_energy
+        self.total_potential_energy.append(total_potential_energy)
+
 
     def update_plot(self):
         for i in range(len(self.particle_list)):
@@ -92,16 +108,43 @@ class MainCycle:
 
     def iterate_cycle(self, time_duration):
         self.calc_forces(self.particle_list)
+        self.calc_potential_energy(self.particle_list)
         for i in range(self.particle_count):
             self.particle_list[i].update()
         self.update_plot()
+
+    def consolidate_min_pe_positions(self):
+        gradient_list = np.gradient(self.total_potential_energy)
+        min_pe_index_list = []
+        for index, value in enumerate(gradient_list):
+            if gradient_list[index-1] < 0 and gradient_list[index+1] > 0:
+                min_pe_index_list.append(index)
+        print(gradient_list)
+        print("min_pe_indexes", min_pe_index_list)
+        particle_dict = dict()
+        time_list = []
+        for i in range(len(self.particle_list)):
+            particle_dict[i] = []
+            for index in min_pe_index_list:
+                particle_dict[i].append(self.particle_list[i].pos[index])
+        for key, value in particle_dict.items():
+            new_list = []
+            for index in range(0, len(value), 2):
+                new_value = (value[index] + value[index+1])/2
+                new_list.append(new_value)
+            particle_dict[key] = new_list
+        for index in min_pe_index_list:
+            time_list.append(self.counter[index])
+        return particle_dict, time_list
 
     def start_cycle(self, time_duration):
         self.set_positions()
         self.plot_sphere()
         self.plot_particles()
         self.set_interval(self.delta_t, self.iterate_cycle, time_duration)
-        plt.plot(self.counter, self.particle_list[0].pos)
+        positions, times = self.consolidate_min_pe_positions()
+        print(positions.values(), times)
+        plt.plot(self.counter, self.total_potential_energy)
         plt.show()
 
 if __name__ == "__main__":
