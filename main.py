641b365c9a00d9b3f855eb60bc72af2caacf47c7
Samuel <samueltan97@hotmail.com>
2020-10-25 10:44:27 -0400

    Adjusted code files

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from time import sleep
 import numpy as np
 import sys
 from particle import Particle
-#from mayavi import mlab
+from mayavi import mlab
 import matplotlib.pyplot as plt
 
 phi = np.linspace(0, 2*np.pi, 100)
@@ -20,8 +20,8 @@ class MainCycle:
         self.make_particle_list()
 
     def call_at_interval(self, period, callback, args):
-        # while self.counter[-1] < int(args[0]):
-        while True:
+	    # while True:
+        while self.counter[-1] < int(args[0]):
             current_counter = round(self.counter[-1] + period, 3)
             self.counter = np.append(self.counter, current_counter)
             sleep(period)
@@ -32,7 +32,7 @@ class MainCycle:
 
     def set_interval(self, period, callback, *args):
         Thread(target=self.call_at_interval, args=(period, callback, args)).start()
-        #mlab.show()
+        # mlab.show()
 
     def make_particle_list(self):  # makes list of N particles
         particle_list = []
@@ -44,8 +44,27 @@ class MainCycle:
         for i in range(len(self.particle_list)):
             set_pos = [np.random.rand(3)]
             # set_pos = [0.3*i, 0.3*i, 0.3*i]
-            self.particle_list[i].pos = (set_pos/np.linalg.norm(set_pos))*1.00
+            self.particle_list[i].pos = (set_pos/np.linalg.norm(set_pos))*1.01
         self.calc_potential_energy(self.particle_list)
+
+    def plot_sphere(self):
+        x = 1 * np.outer(np.cos(phi), np.sin(theta))
+        y = 1 * np.outer(np.sin(phi), np.sin(theta))
+        z = 1 * np.outer(np.ones(np.size(phi)), np.cos(theta))
+        mlab.mesh(x, y, z, colormap="Spectral") 
+
+    def plot_particles(self): # plots INITIAL positions of particles
+        particle_plots = []
+        force_plots = []
+        for i in range(len(self.particle_list)):
+            x = 0.05 * np.outer(np.cos(phi), np.sin(theta)) + self.particle_list[i].pos[0][0]
+            y = 0.05 * np.outer(np.sin(phi), np.sin(theta)) + self.particle_list[i].pos[0][1]
+            z = 0.05 * np.outer(np.ones(np.size(phi)), np.cos(theta)) + self.particle_list[i].pos[0][2]
+            if i == 0:
+                particle_plots.append(mlab.mesh(x, y, z, colormap="PuBu"))
+            else:
+                particle_plots.append(mlab.mesh(x, y, z, colormap="autumn"))
+        self.particle_plots = particle_plots 
 
     def calc_forces(self, particle_list):
         for i in range(len(particle_list)):
@@ -93,30 +112,9 @@ class MainCycle:
         self.calc_potential_energy(self.particle_list)
         for i in range(self.particle_count):
             self.particle_list[i].update()
-        print(self.total_potential_energy[-1])
-        #self.update_plot()
+        # self.update_plot()
 
-    ''' def plot_sphere(self):
-            x = 1 * np.outer(np.cos(phi), np.sin(theta))
-            y = 1 * np.outer(np.sin(phi), np.sin(theta))
-            z = 1 * np.outer(np.ones(np.size(phi)), np.cos(theta))
-            mlab.mesh(x, y, z, colormap="Spectral") '''
-
-    ''' def plot_particles(self): # plots INITIAL positions of particles
-            particle_plots = []
-            force_plots = []
-            for i in range(len(self.particle_list)):
-                x = 0.05 * np.outer(np.cos(phi), np.sin(theta)) + self.particle_list[i].pos[0][0]
-                y = 0.05 * np.outer(np.sin(phi), np.sin(theta)) + self.particle_list[i].pos[0][1]
-                z = 0.05 * np.outer(np.ones(np.size(phi)), np.cos(theta)) + self.particle_list[i].pos[0][2]
-                if i == 0:
-                    particle_plots.append(mlab.mesh(x, y, z, colormap="PuBu"))
-                else:
-                    particle_plots.append(mlab.mesh(x, y, z, colormap="autumn"))
-            self.particle_plots = particle_plots 
-    '''
-
-    '''def consolidate_min_pe_positions(self):
+    def consolidate_min_pe_positions(self):
         gradient_list = np.gradient(self.total_potential_energy)
         min_pe_index_list = []
         for index, value in enumerate(gradient_list):
@@ -138,17 +136,17 @@ class MainCycle:
             particle_dict[key] = new_list
         for index in min_pe_index_list:
             time_list.append(self.counter[index])
-        return particle_dict, time_list'''
+        return particle_dict, time_list
 
     def start_cycle(self, time_duration):
         self.set_positions()
-       # self.plot_sphere()
-        #self.plot_particles()
+        # self.plot_sphere()
+        # self.plot_particles()
         self.set_interval(self.delta_t, self.iterate_cycle, time_duration)
-        #positions, times = self.consolidate_min_pe_positions()
-        #print(positions.values(), times)
-        #plt.plot(self.counter, self.total_potential_energy)
-        # plt.show()
+        positions, times = self.consolidate_min_pe_positions()
+        print(positions.values(), times)
+        plt.plot(self.counter, self.total_potential_energy)
+        plt.show()
 
 if __name__ == "__main__":
     # first arg will be number of particles and second arg will be delta T in seconds and third arg will be total duration in seconds
